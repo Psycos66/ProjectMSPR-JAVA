87fdc493317bd0be9e3b6c454a146a722677ae21
Code psn 50 <matbelin5@gmail.com>
2022-02-09 16:04:06 +0100

    v1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,93 +1,104 @@
 package com.company;
 import java.io.*;
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        String file = "C:\\Users\\Matbe\\Desktop\\fichier.txt";
+
+        List<String>ListText = new ArrayList<>();
+        File repertoire = new File("html/Cagent/FichierTXT");
+        String liste[] = repertoire.list();
+
+        if (liste != null) {
+            for (int i = 0; i < liste.length; i++) {
+                ListText.add(liste[i]);
+            }
+        } else {
+            System.err.println("Nom de repertoire invalide");
+        }
+
+        for(String FileName : ListText)
+        {
+            System.out.println(FileName);
+        }
+
+
+        String file = "sauvegarde/Agent/agent.txt";
+        String file2 = "sauvegarde/Equipement/equipement.txt";
+
+        List<String>ListEquipement = new ArrayList<>();
+
         try(BufferedReader br = new BufferedReader(new FileReader(file)))
         {
             String line;
-            FileOutputStream fs = new FileOutputStream("C:\\Users\\Matbe\\Desktop\\Xefi Academy\\Projet\\Projet Js\\ProjetMSPR\\html\\Agent\\listeAgents.html");
+            int y = 1;
+            FileOutputStream fs = new FileOutputStream("html/Agent/listeAgents.html");
             OutputStreamWriter out = new OutputStreamWriter(fs);
+
+
             out.write("<html>");
             out.write("<body>");
 
             while ((line = br.readLine()) != null) {
                 System.out.println(line);
-                out.write("<li>" + line + "</li>");
+                out.write("<li><a href='file:///html/Cagent/Cagent" + y + ".html'>" + line + "</a></li>");
+                y++;
             }
             out.write("</body>");
             out.write("</html>");
             out.close();
 
+            try(BufferedReader br2 = new BufferedReader(new FileReader(file2)))
+            {
+                String line2;
+
+                while ((line2 = br2.readLine()) != null) {
+                    ListEquipement.add(line2);
+                }
+                System.out.println(ListEquipement);
+            }
+
+            try
+            {
+                for(String FileName : ListText)
+                {
+                    String file3 = "html/Cagent/Cagent" + FileName;
+                    BufferedReader br3 = new BufferedReader(new FileReader(file3));
+                    String line3;
+
+                    FileOutputStream fs2 = new FileOutputStream("html/Cagent/Cagent/" + FileName + ".html");
+                    OutputStreamWriter out2 = new OutputStreamWriter(fs2);
+
+                    while ((line3 = br3.readLine()) != null) {
+                        for(String equip : ListEquipement)
+                        {
+                            if(line3.equals(equip))
+                            {
+                                out2.write("<li>" + equip + "<input type='checkbox' id='scales' checked><br></li> ");
+                            }
+                            else
+                            {
+                                out2.write("<li>" + equip + "<input type='checkbox' id='scales'><br></li> ");
+                            }
+                        }
+                    }
+                    out2.close();
+                }
+            }
+            catch (IOException e)
+            {
+
+            }
         }
-        catch (IOException e) {
+
+        catch (IOException e)
+        {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
 
-    }
 
-    /*public static String textToHTML(String text)
-    {
-        if(text == null) {
-            return null;
-        }
-        int length = text.length();
-        boolean prevSlashR = false;
-        StringBuffer out = new StringBuffer();
-        for(int i = 0; i < length; i++) {
-            char ch = text.charAt(i);
-            switch(ch) {
-                case '\r':
-                    if(prevSlashR) {
-                        out.append("<br>");
-                    }
-                    prevSlashR = true;
-                    break;
-                case '\n':
-                    prevSlashR = false;
-                    out.append("<br>");
-                    break;
-                case '"':
-                    if(prevSlashR) {
-                        out.append("<br>");
-                        prevSlashR = false;
-                    }
-                    out.append("&quot;");
-                    break;
-                case '<':
-                    if(prevSlashR) {
-                        out.append("<br>");
-                        prevSlashR = false;
-                    }
-                    out.append("&lt;");
-                    break;
-                case '>':
-                    if(prevSlashR) {
-                        out.append("<br>");
-                        prevSlashR = false;
-                    }
-                    out.append("&gt;");
-                    break;
-                case '&':
-                    if(prevSlashR) {
-                        out.append("<br>");
-                        prevSlashR = false;
-                    }
-                    out.append("&amp;");
-                    break;
-                default:
-                    if(prevSlashR) {
-                        out.append("<br>");
-                        prevSlashR = false;
-                    }
-                    out.append(ch);
-                    break;
-            }
-        }
-        return out.toString();
-    }*/
+    }
 }
