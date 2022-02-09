8c1146b538e489c1e3dd8eb42f30ce277b40eecc
moreauc-coder <GGu_est@outlook.fr>
2022-02-09 14:40:37 +0100

    php

---
+++ html/Vue/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="{{asset('images/xefi.ico')}}">
+    <title>Accueil</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <!-- Styles -->
+
+</head>
+<body>
+    <?php
+    include '../Agent/listeAgents.html';
+    ?> 
+</body>
+</html>
