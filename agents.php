3352c56b5b10202e11e90ce5f18384f8270068b6
JulienS69 <julien.seux69@gmail.com>
2022-02-10 11:32:57 +0100

    [WEB] Ajout du css front web

--- agents.php
+++ agents.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="assets/image/logo.ico">
-    <title>Accueil</title>
+    <title>Liste des agents</title>
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
@@ -18,50 +18,54 @@
     <link rel="stylesheet" href="assets/css/main.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,b%7CRoboto+Condensed:300,400,b%7CRoboto:300,400,b">
 </head>
-
 <body>
-    <div class="row nav-row">
-        <div class="col-xs-12">
-            <div class="container-fluid nav-container"><a href="index.php" class="responsive-picture logo" title="EPSI LOGO"><picture><img alt="EPSI" src="assets/image/logoepsi-400x400.png"></picture></a>
-                <a class="link-text nav-link2" href="agents.php" title="Liste des agents">Liste des agents</a>
-                <a class="link-text nav-link2" href="index.php" title="Espace Eleve">Présentation du projet</a>
-            </div>
+<div class="row nav-row">
+    <div class="col-xs-12">
+        <div class="container-fluid nav-container"><a href="index.php" class="responsive-picture logo" title="EPSI LOGO"><picture><img alt="EPSI" src="assets/image/logoepsi-400x400.png"></picture></a>
+            <a class="link-text nav-link2" href="agents.php" title="Liste des agents">Liste des agents</a>
+            <a class="link-text nav-link2" href="index.php" title="Espace Eleve">Présentation du projet</a>
         </div>
     </div>
-    <div class="row wecome-row">
-        <div class="col-xs-12 welcome-column">
-            <h4 class="welcome-subtitle">Projet</h4>
-            <h1>MSPR</h1>
-        </div>
+</div>
+<div class="row wecome-row">
+    <div class="col-xs-12 welcome-column">
+        <h4 class="welcome-subtitle">Liste des</h4>
+        <h1>Agents</h1>
     </div>
+</div>
 </body>
+<br>
 <br>
 <br>
 <br>
 <br>
 <body>
 <div style="border: 2px solid #659224; border-radius:10px; padding:40px;">
+    <div style="display: flex;
+    width: 100%;
+    justify-content: center;
+    align-items: center; ">
+        <h1 style=" font-size:medium; font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; margin-right:auto; margin-left:auto;">Agents de sécurité
+        </h1>
+        <img src="assets/image/ecusson-rond-securite-privee.jpg" style="border-radius:10px; margin-bottom:5px; border: 2px solid #659224; padding:5px; max-width:5rem;"/>
 
-<h3 style="color: #659224">
-    Présentation du projet  :
-</h3>
-    <p style="color: #659224; font-weight: bold "> Le projet consiste à : <p>
-    <p> Configurer un serveur web avec un accès ssh pour sa maintenance et le déploiement des fichiers
-        statiques  </p>
-    <p> • Écrire d’un ou plusieurs <strong style="color: #379EC1"> programmes java </strong> destinés à générer les fichiers du serveur web. </p>
-    <p> Leurs sources seront stockées sur le dépôt <strong style="color: #379EC1"> git </strong> de sauvegarde </p>
-    <p>   • Configurer un serveur <strong style="color: #379EC1">Jenkins</strong> capable d’accéder au dépôt de sauvegarde et au serveur web </p>
-    <p>   • Concevoir un ou plusieurs <strong style="color: #379EC1"> pipeline(s) Jenkins de génération </strong> de la ou des application(s) java </p>
-    <p>     • Concevoir le pipeline Jenkins de génération et déploiement des fichiers du <strong style="color: #379EC1"> serveur web </strong>
-    </p>
-</body>
+    </div>
+    <div style="border: 2px solid #379EC1; border-radius:10px;padding:30px;">
+        <?php
+        include 'html/Agent/listeAgents.html';
+        ?>
+    </div>
 
 </div>
 <br>
 <br>
 <br>
 <br>
-
+</body>
+<br>
+<br>
+<br>
+<br>
 <div class="footer-basic" style="background-color: #333333">
     <footer>
         <div class="social" style="color:#FFFFFF;"><a href="https://github.com/Psycos66/ProjectMSPR-JAVA"><i class="icon ion-social-github"></i></a></div>
